abd835d75fc389ce71d2e5e96f7ce636a3a984af
criptik <tom.deneau@gmail.com>
2020-04-05 23:20:44 -0500

    most of chapter 4 of miguel flask tutorial, except for Registration

--- flask/hello/app/routes.py
+++ flask/hello/app/routes.py
@@ -1,9 +1,12 @@
 from app import app
 from flask import Flask
-from flask import request, render_template, redirect, flash
+from flask import request, render_template, redirect, flash, url_for
 import pprint
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
+# junk from original hello
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
@@ -29,8 +32,8 @@ def about():
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Miguel'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -41,15 +44,25 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    if current_user.is_authenticated:
+        flash('User %s is already logged in' % (current_user.username))
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user %s, remember_me=%s'
-              % (form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
