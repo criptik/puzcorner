2ddffdb76f49ad539356c7f3328289c60718ce99
criptik <tom.deneau@gmail.com>
2020-04-06 16:14:47 -0500

    flask hello sample, finished registration part

--- flask/hello/app/routes.py
+++ flask/hello/app/routes.py
@@ -5,6 +5,8 @@ import pprint
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from app import db
+from app.forms import RegistrationForm
 
 # junk from original hello
 @app.route('/hello', methods=['GET', 'POST'])
@@ -66,3 +68,20 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+# ...
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
