1ed092d1fab1ea58dde14fa23b53bf476b07c0d8
criptik <tom.deneau@gmail.com>
2020-04-07 17:54:30 -0500

    Flask Tut Chap 6 but no Profile Editor yet

--- flask/hello/app/routes.py
+++ flask/hello/app/routes.py
@@ -8,6 +8,14 @@ from app.models import User
 from app import db
 from app.forms import RegistrationForm
 
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
 # junk from original hello
 @app.route('/hello', methods=['GET', 'POST'])
 def hello_world():
@@ -85,3 +93,13 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
