91372e97c450aa4c113dfa160414e7c9911f4d1a
Tom Deneau <tom.deneau@gmail.com>
2022-06-11 18:37:07 -0500

    rotatenum.py: checkpoint, RotateRightTwo

--- rotatenum.py
+++ rotatenum.py
@@ -58,8 +58,60 @@ class SolveRotateRightOne(SolveBase):
             else:
                 print(f'bad {start}')
 
-    
+class SolveRotateRightTwo(SolveBase):
+    # generate the number which is the argument rotated two to the right
+    def rotated(self, n):
+        nstr = self.toStr(n)
+        sliceamt = len(nstr) - 2
+        first = nstr[0:sliceamt]
+        second = nstr[sliceamt:]
+        result = second + first
+        return int(result, self.base)
+
+    def getSolution(self):
+        print('base=', self.base, 'mult=', self.mult, '---------')
+        # for start in range(34, 35):
+        for start in range(1, self.base**2):
+            d = start
+            carry = 0
+            a = 0
+            exp = 0
+            lastd = 0
+            # print(f'before loop, exp={exp}, d={d}, carry={carry}  ')
+            while True:
+                a = a + d*self.base**exp
+                lastd = d
+                # print(f'exp={exp}, a={self.toStr(a)}')
+                # compute next d
+                d = d * self.mult + carry
+                carry = d // self.base**2
+                d = d % self.base**2
+                exp = exp + 2
+                # avoid an endless loop
+                if exp > self.base**2 * self.mult:
+                    break
+                # print(f'exp={exp}, d={d}, carry={carry}  ')
+                # if we get back to start with carry==0, we're good
+                if d == start and carry == 0:
+                    break
+
+            r = self.rotated(a)
+
+            if (r == a*self.mult and lastd != 0):
+                print(f'ok  {start} {self.toStr(a)} {self.toStr(r)} ({len(self.toStr(a))})')
+            else:
+                pass
+                # print(f'bad {start}')
+
+
+
+if False:
+    solver = SolveRotateRightTwo(8, 4)
+    solver.getSolution()
+    sys.exit(1)
+
 for base in range(3, 11):
     for mult in range(2, base):
-        solver = SolveRotateRightOne(base, mult)
+        # solver = SolveRotateRightOne(base, mult)
+        solver = SolveRotateRightTwo(base, mult)
         solver.getSolution()
