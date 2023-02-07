9dfbd39a7c0c7348f7687e80ca1ab653b6d3e526
Tom Deneau <tom.deneau@gmail.com>
2023-02-07 12:24:13 -0600

    tweaks to rotatenum.py

--- rotatenum.py
+++ rotatenum.py
@@ -1,12 +1,25 @@
 import sys
-# note: the brute force search thru all numbers is way too slow!
+# M/J2. Arthur Wasserman imagines taking a positive integer
+# and moving its last digit to the front. For example 1,234 would
+# become 4,123. Wasserman wants to know: What is the smallest
+# positive integer such that when you do this, the result is exactly
+# double the original?
+
+# note: This uses a more intelligent search than the brute force search
+# which searches thru all numbers and is way too slow!
 
 class SolveBase:
-    def __init__(self, base, mult):
+    def __init__(self, base, mult, useBruteForce=False):
         if (base > 62):
-            print('base not supported:', base);
+            print('base not supported:', base)
+            sys.exit(-1)
+        if (mult >= base):
+            print('mult must be less than base', mult, base)
+            sys.exit(-1)
         self.base = base
         self.mult = mult
+        self.useBruteForce = useBruteForce
+        self.bruteExpLimit = 8
         self.verbose = False
         
     # generate the string for the number in the base we are using
@@ -23,19 +36,64 @@ class SolveBase:
 
     def toInt(self, str):
         return int(str, self.base)
-    
-    def showSolDetail(self, a, r):
-        astrlen = len(self.toStr(a))
-        oddtxt = 'odd length' if (astrlen % 2 == 1) else ''
-        print(f'{self.toStr(a)} * {self.mult} = {self.toStr(r)} ({astrlen}) {oddtxt}')
 
-class SolveRotate(SolveBase):
-    def __init__(self, base, mult, rotateamt):
-        super(SolveRotate, self).__init__(base, mult)
-        self.rotateamt = rotateamt
+    def className(self):
+        return type(self).__name__
+    
+    def paramStr(self):
+        return(f'class {self.className()}, base {self.base}, mult {self.mult}, useBruteForce={self.useBruteForce}')
 
     def getMinSolution(self):
-        minval = 0
+        self.minval = 0
+        self.gaveWarning = False
+        if self.useBruteForce:
+            self.minval = self.getMinSolutionBruteForce()
+        else:
+            self.minval = self.getMinSolutionSmart()
+        # we finished all the start values show which had the minimum:
+        if self.minval == 0:
+            print(f' no solutions found for {self.paramStr()}')
+            return 0
+        else:
+            minrot = self.rotated(self.minval)
+            print(f'min for {self.paramStr()}: ', end='')
+            self.showSolDetail(self.minval, minrot)
+            return self.minval
+
+    # by default, there is no "smart" solution unless overridden in child
+    def getMinSolutionSmart(self):
+        print(f'Smart Solution not supported in class {self.className()}') 
+        return 0
+    
+    def getMinSolutionBruteForce(self):
+        self.minval = 0
+        # numbers to test, we can skip values that would increase the number of digits
+        exp=1
+        print(f'exp=', end='')
+        while exp < self.bruteExpLimit:
+            print(f'{exp} ', end='')
+            sys.stdout.flush()
+            for n in range(self.base**exp, self.base**(exp+1)//self.mult):
+                if n * self.mult == self.rotated(n):
+                    return n
+            exp = exp + 1
+        print('')
+        return 0
+
+    def showSolDetail(self, a, r):
+        astrlen = len(self.toStr(a))
+        print(f'{self.toStr(a)} * {self.mult} = {self.toStr(r)} ({astrlen})')
+
+class SolveRotate(SolveBase):
+    def __init__(self, base, mult, rotateamt=1, useBruteForce=False):
+        super(SolveRotate, self).__init__(base, mult, useBruteForce)
+        self.rotateamt = rotateamt
+
+    def paramStr(self):
+        return(f'{super(SolveRotate, self).paramStr()}, rotateamt={self.rotateamt}')
+        
+    def getMinSolutionSmart(self):
+        self.minval = 0
         for start in range(1, self.base**self.rotateamt):
             if (self.verbose):
                 print(f'base={self.base}, mult={self.mult}, start={start}')
@@ -52,16 +110,9 @@ class SolveRotate(SolveBase):
                     if self.verbose:
                         print(f'base {self.base}, mult {self.mult}: ', end='')
                         self.showSolDetail(a, r)
-                    minval = a if minval == 0 or a < minval else minval
-
-        # we finished all the start values show which had the minimum:
-        if minval == 0:
-            print(f' no solutions found for base {self.base}, mult {self.mult}')
-        else:
-            minrot = self.rotated(minval)
-            print(f'min for base {self.base}, mult {self.mult}, rotateamt={self.rotateamt}: ', end='')
-            self.showSolDetail(minval, minrot)
-
+                    # record it and continue in case a higher start value yields a smaller value
+                    self.minval = a if self.minval == 0 or a < self.minval else self.minval
+        return self.minval
 
     
 class SolveRotateRight(SolveRotate):
@@ -90,10 +141,8 @@ class SolveRotateRight(SolveRotate):
         carry = 0
         a = 0
         exp = 0
-        lastd = 0
         while True:
             a = a + (d * self.base**exp)
-            lastd = d
             # print(f'exp={exp}, a={self.toStr(a)}')
             # compute next d
             d = d * self.mult + carry
@@ -111,13 +160,6 @@ class SolveRotateRight(SolveRotate):
                     return newa
 
 class SolveRotateLeft(SolveRotate):
-    def __init__(self, base, mult, rotateamt):
-        # for now only support rotateamt==1
-        if (rotateamt != 1):
-            print(f'RotateLeft does not support rotateamt={rotateamt}')
-            sys.exit(1)
-        super(SolveRotateLeft, self).__init__(base, mult, rotateamt)
-        
     # generate the number which is the argument rotated the correct amount to the left
     def rotated(self, n):
         nstr = self.toStr(n)
@@ -131,7 +173,11 @@ class SolveRotateLeft(SolveRotate):
             print(f'curstr={curstr}, carryin={carryin}, start={start}')
         if (len(curstr) > self.base * self.mult):
             return -1
-        lastd = int(curstr[-1])
+        lastdchr = curstr[-1]
+        #detect stuck in a series of repeating digits
+        if (len(curstr) > 3) and curstr[-2] == lastdchr and curstr[-3] == lastdchr and curstr[-4] == lastdchr:
+            return -1
+        lastd = self.toInt(curstr[-self.rotateamt:])
         nextd = self.mult * lastd
         nextdcarry = nextd // self.base
         if (nextdcarry != carryin):
@@ -147,8 +193,6 @@ class SolveRotateLeft(SolveRotate):
             newcurstr = curstr + self.toStr(nextd + testcarry) 
             result = self.finishSolution(newcurstr, testcarry, start)
             if result == -1:
-                if self.verbose:
-                    print(f'result={result}')
                 next
             else:
                 return result
@@ -156,20 +200,48 @@ class SolveRotateLeft(SolveRotate):
         return -1
 
     def getSolutionForStart(self, start):
+        # for now only support rotateamt==1
+        if (self.rotateamt != 1):
+            if not self.gaveWarning:
+                print(f'Warning: RotateLeftSmart does not support rotateamt={self.rotateamt}')
+                self.gaveWarning = True
+            return -1
+            # sys.exit(1)
         a = self.finishSolution(self.toStr(start), 0, start)
         return a
-    
+
+class SolveReversed(SolveBase):
+    def rotated(self, n):
+        nstr = self.toStr(n)
+        revstr = "".join(reversed(nstr))
+        return self.toInt(revstr)
 
 #-------- main code starts here ---------
 if False:
-    solver = SolveRotateLeft(5, 4, 1)
-    print(solver.toStr(12345678), solver.toStr(solver.rotated(12345678)))
-    result = solver.getMinSolution()
-    print(f'result={result}')
+    base = 8
+    useRotateRight = True
+    rotateChoices = range(1, 4) if useRotateRight else [1]
+    showBadCompare = False
+    printedRotate = False
+    for rotamt in rotateChoices:
+        for mult in range(2,base):
+            if useRotateRight:
+                solver = SolveRotateRight(base, mult, rotamt)
+            else:
+                solver = SolveReversed(base, mult)
+            if not printedRotate:
+                n = 4*25 + 3*5 + 2
+                print(solver.toStr(n), solver.toStr(solver.rotated(n)))
+                printedRotate = True
+            resultSmart = solver.getMinSolution()
+            solver.useBruteForce = True
+            resultBrute = solver.getMinSolution()
+            if (resultSmart != resultBrute and showBadCompare):
+                print(f'Results Differ: smart={solver.toStr(resultSmart)}, brute={solver.toStr(resultBrute)}')
     sys.exit(1)
 
-rotateamt = 2
+rotateamt = 1
 for base in range(2, 11):
     for mult in range(2, base):
-        solver = SolveRotateLeft(base, mult, rotateamt)
+        solver = SolveRotateRight(base, mult, rotateamt)
         solver.getMinSolution()
