a0211787b3223edbe254ef138ea492056a539bf6
Tom Deneau <tom.deneau@gmail.com>
2024-10-16 15:50:56 -0500

    three Heirs problem

--- ctofgen.py
+++ ctofgen.py
@@ -1,113 +1,174 @@
 import collections
 import sys
 import argparse
+import math
+# import numpy
 
 # just some brute force searches for various patterns
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='C to F brute force searcher')
-    parser.add_argument('--low', type=int, default=1000, help='low val for search')
+    parser.add_argument('--low', type=int, default=1, help='low val for search')
     parser.add_argument('--high', type=int, default=100000, help='high val for search')
     parser.add_argument('--stopOnFirst', default=False, action='store_true', help='whether to stop on first solution')
+    parser.add_argument('--test', default=False, action='store_true', help='just test some target generators')
     parser.add_argument('--searcher', default='ShuffleDigits', help='searcher class to use')
-    
+
     return parser.parse_args()
 
 def str_to_class(classname):
     return getattr(sys.modules[__name__], classname)    
 
 
-class ShuffleDigits:
-    def isTrue(self, C, F):
-        counterF = collections.Counter(list(str(F)))
-        counterC = collections.Counter(list(str(C)))
-        return counterF == counterC
+class SearcherBase:
+    def __init__(self):
+        self.tries = 0
+        self.matches = 0
+
+    def endProgram(self):
+        matchWordEnd = '' if self.matches == 1 else 'es'
+        print(f'{self.matches} match{matchWordEnd} out of {self.tries} tries')
+        sys.exit(0)
     
+    # return 1, 0, or -1
+    def signof(self, x):
+        return (x > 0) - (x < 0)
+
+    def isMatch(self, C, F):
+        # print(C, F, self.targetNumber(C))
+        return F == self.targetNumber(C)
+
+    def nextC(self, C):
+        return C + self.step()
+
+    def advanceToModStart(self, C):
+        while C % 10 not in searcher.modStartList():
+            C += 1
+        return C
+
+    def cToF(self, C):
+        return 9*C//5 + 32
+
+    def fToC(self, F):
+        return (F-32)*5 // 9
+
+    # default step and modStartList allow any C ending in 0 or 50
     def step(self):
         return 5
 
     def modStartList(self):
         return [0,5]
 
-class ReverseDigits:
-    def isTrue(self, C, F):
-        return F == self.reverseNumber(C)
+    # utility routine when F high has a certain requirement
+    def nextCforHighFReq(self, C, highFReq):
+        # for cases where F high digit must be a certain value
+        # if F is too big, make big adjustment
+        F = self.cToF(C)
+        numlen = len(str(abs(F)))
+        # get highest digit
+        pow10 = (10 ** (numlen-1))
+        highestDigitF = abs(F) // pow10
+        
+        if C > 0 and highestDigitF > highFReq:
+            newF = pow10 * 10 * highFReq
+            newC =  searcher.fToC(newF) - 10
+            newC = self.advanceToModStart(newC)
+            # print(C, F, newC, self.cToF(newC))
+            return newC
+        else:
+            return C + self.step()
 
-    def reverseNumber(self, number):
-        numList = list(str(number))
+# class which requires Clow and Fhigh to be 5    
+class SearcherBaseCLowFHigh5(SearcherBase):    
+    def step(self):
+        return 10
+
+    def modStartList(self):
+        return [5]
+
+    def nextC(self, C):
+        return self.nextCforHighFReq(C, 5)
+    
+    
+class ShuffleDigits(SearcherBase):
+    def isMatch(self, C, F):
+        counterF = collections.Counter(list(str(F)))
+        counterC = collections.Counter(list(str(C)))
+        return counterF == counterC
+    
+class ReverseDigits(SearcherBaseCLowFHigh5):
+    def targetNumber(self, number):
+        sgn = self.signof(number)
+        absnum = abs(number)
+        numList = list(str(absnum))
         reversedNum = int(''.join(numList[::-1]))
-        return reversedNum
+        return reversedNum * sgn
     
-    def step(self):
-        return 10
-
-    def modStartList(self):
-        return [5]
+class RotateRight(SearcherBaseCLowFHigh5):
+    def targetNumber(self, number):
+        sgn = self.signof(number)
+        absnum = abs(number)
+        numlen = len(str(absnum))
+        # get lowest digit
+        lowest = absnum % 10
+        return (absnum // 10 + lowest * (10 ** (numlen-1))) * sgn
     
-class RotateRight:
-    def isTrue(self, C, F):
-        # print(C, F, self.rotateRightNumber(C))
-        return F == self.rotateRightNumber(C)
-
-    def rotateRightNumber(self, number):
-        numList = list(str(number))
-        tmp = numList[-1]
-        for n in range(len(numList)-1, 0, -1):
-            numList[n] =  numList[n-1]
-        numList[0] = tmp
-        rotatedNum = int(''.join(numList))
-        return rotatedNum
+class RotateLeft(SearcherBase):
+    def targetNumber(self, number):
+        sgn = self.signof(number)
+        absnum = abs(number)
+        numlen = len(str(absnum))
+        # get highest digit
+        pow10 = (10 ** (numlen-1))
+        highest = absnum // pow10
+        return ((absnum % pow10)  * 10 + highest) * sgn
     
-    def step(self):
-        return 10
-
-    def modStartList(self):
-        return [5]
-    
-class SwapFirstLast:
-    def isTrue(self, C, F):
-        # print(C, F, self.rotateRightNumber(C))
-        return F == self.swapFirstLastNumber(C)
-
-    def swapFirstLastNumber(self, number):
-        numList = list(str(number))
+class SwapFirstLast(SearcherBaseCLowFHigh5):
+    def targetNumber(self, number):
+        sgn = self.signof(number)
+        absnum = abs(number)
+        numList = list(str(absnum))
         tmp = numList[0]
         numList[0] = numList[-1]
         numList[-1] = tmp
         swappedNum = int(''.join(numList))
-        return swappedNum
-    
-    def step(self):
-        return 10
-
-    def modStartList(self):
-        return [5]
+        return swappedNum * sgn
     
 
-def endProgram():
+# main code:
+        
+args = parse_args()
+if args.low < -273.15:
+    print("low value adjusted to absolute 0 (-273 C)")
+    args.low = -273
+if args.test:
+    for S in [RotateLeft(), RotateRight(), ReverseDigits(), SwapFirstLast()]:
+        print(S)
+        for tst in [1234, 12345, 123450, 12345678901234567890]:
+            for x in [tst, -1*tst]:
+                print(x, S.targetNumber(x))
     sys.exit(0)
     
-# main code:
-#for tst in [1234, 12345]:
-#    print(tst, swapFirstLastNumber(tst))
-#    print(tst, reverseNumber(tst))
-
-args = parse_args()
 searcher = str_to_class(args.searcher)()
 C = args.low
 # advance until C meets the modstart requirement
-while C % 10 not in searcher.modStartList():
-    C += 1
-# print(C)
+C = searcher.advanceToModStart(C)
 
 while C < args.high:
-    #if C % 10000000 in searcher.modStartList():
+    searcher.tries += 1
+    if searcher.tries % 1000000 == 0:
+        print(f'...{searcher.tries}\r', end='')
+    #if C % 1000 in searcher.modStartList():
     #    print(f'{C}...')
-    F = 9 * C // 5 + 32
-    if searcher.isTrue(C, F):
+    F = searcher.cToF(C)
+    if searcher.isMatch(C, F):
+        searcher.matches += 1
         print(args.searcher, C, F)
         if args.stopOnFirst:
-            endProgram()
+            break
             
-    C += searcher.step()
+    C = searcher.nextC(C)
+
+
+searcher.endProgram()
---
+++ threeHeirs.py
@@ -0,0 +1,168 @@
+import collections
+import sys
+
+
+# puzzle statement
+# Three Heirs (contributed by Kaustuv Sengupta )
+       #        A wealthy man had three adult children all of whom were
+       # skilled at math and logic. To get a share of his inheritance
+       # each had to correctly determine a secret number which he had
+       # chosen. He told them that the number had four different
+       # non-zero decimal digits, in ascending order.  He prepared three
+       # sealed envelopes each of which contained a clue. Envelope #1
+       # contained the product of the four digits, envelope #2 contained
+       # the sum of the squares of the four digits, and envelope #3
+       # contained the sum of the product of the first two digits and
+       # the product of the last two digits. For example, if the number
+       # were 1234, the first envelope would contain 1×2×3×4=24, the
+       # second envelope would contain 12+22+32+42=30, and the third
+       # envelope would contain 1×2+3×4=14. The envelopes were clearly
+       # marked. Each child was given one envelope. They each saw the
+       # number inside their own envelopes but could not see the numbers
+       # inside the other two envelopes.  Then he explained the
+       # rules. After one hour each child could either submit a number
+       # or decline. Anyone who submitted a wrong answer would be
+       # eliminated and get nothing. If one or more submitted the
+       # correct answer they would receive equal shares of the
+       # inheritance. The contest would then end, with the others
+       # getting nothing. If no one submitted the correct answer they
+       # would be instructed to work on the problem for another
+       # hour. The process would repeat as often as necessary. Each of
+       # the children decided not to submit an answer unless they sure
+       # it was correct.  The children were placed in different rooms
+       # where they could not communicate with one another. At the end
+       # of the first hour no one had submitted an answer. At the end of
+       # the second hour no one had submitted an answer. At the end of
+       # the third hour no one had submitted an answer. At the end of
+       # the fourth hour all three of them submitted the correct answer!
+       # What number had the father chosen?
+
+debug = False
+
+def dbglog(s):
+    if debug:
+        print(s)
+        
+class NumInfo:
+    def __init__(self, a, b, c, d):
+        self.num = a*1000 + b*100 + c*10 +d
+        self.prod = a * b * c * d
+        self.sumsq = a**2 + b**2 + c**2 + d**2
+        self.sumprod = a*b + c*d
+        prodDict.addToDict(self.prod, self)
+        sumsqDict.addToDict(self.sumsq, self)
+        sumprodDict.addToDict(self.sumprod, self)
+
+    def __str__(self):
+        return f'{self.num}: {self.prod}  {self.sumsq}  {self.sumprod}'
+
+
+class MyDict:
+    def __init__(self, name):
+        self.mydict = {}
+        self.reverseDict = {}
+        self.name=name
+
+    def addToDict(self, key, info):
+        if key in self.mydict:
+            self.mydict[key].append(info)
+        else:
+            self.mydict[key] = [info]
+        self.reverseDict[info.num] = key
+
+    def infoListStr(self, infoList):
+        str = ''
+        if len(infoList) == 0:
+            str += '[]'
+        else:
+            str += '['
+            for info in infoList:
+                str += f'{info.num} '
+            str += ']'
+        return str
+    
+        
+    def removeNum(self, key, num):
+        # dbglog(f'{self.name}: removeNum {key} {num}')
+        if not key in self.mydict:
+            dbglog(f' ...could not find key={key} in {self.name}')
+            return
+        for idx in range(0, len(self.mydict[key])):
+            # dbglog(f'{self.name}: removeNumid {key} {num} {idx} {self.mydict[key][idx].num}')
+            if self.mydict[key][idx].num == num:
+                del self.mydict[key][idx]
+                # if shortened list is down to length 0, remove mapping
+                if len(self.mydict[key]) == 0:
+                    del self.mydict[key]
+                    dbglog(f' {self.name} removing {key}->{num}')
+                    return
+                else:
+                    dbglog(f' {self.name} shortening list for {key} to {self.infoListStr(self.mydict[key])}')
+                    return
+        dbglog(f'  {self.name} did not match {num} in list for {key}')
+        return
+        # if we got this far, list exists but num is not in it, just return
+        dbglog(f' for key {key}, {num} not found in list')
+        return
+                        
+    def buildLen1KeyDict(self):
+        # builds key to info mapping
+        len1dict = {}
+        for key in self.mydict:
+            if len(self.mydict[key]) == 1:
+                len1dict[key] = self.mydict[key][0]
+        self.len1KeyDict = len1dict
+        return len1dict
+                
+        
+    def removeFromAll3(self, key, info, prodDict, sumsqDict, sumprodDict):
+        num = info.num
+        dictKeys = ((prodDict, info.prod), (sumsqDict, info.sumsq), (sumprodDict, info.sumprod))
+        for (remdict, key) in dictKeys:
+            #if num == 3578:
+            #    print(f'{remdict.name}, {key}, {num}')
+            #    print(dictKeys)
+            remdict.removeNum(key, num)
+            #if key == 840:
+            #    print(f'finished {remdict.name} {key}')
+    def show(self):
+        print(f'{self.name}:')
+        for key in self.mydict:
+            print(f'key={key}: [', end="")
+            ary = self.mydict[key]
+            for info in ary:
+                print(f'{info.num} ', end="")
+            print(']')
+        
+        
+prodDict = MyDict('prodDict')
+sumsqDict = MyDict('sumsqDict')
+sumprodDict = MyDict('sumprodDict')
+dicts = [prodDict, sumsqDict, sumprodDict]
+
+
+for a in range(1,10):
+    for b in range(a+1, 10):
+        for c in range(b+1, 10):
+            for d in range(c+1, 10):
+                info = NumInfo(a,b,c,d)
+                # sys.exit(0)
+                # print(f'{a}{b}{c}{d}   {prod:4d}  {sumsq:4d}  {sumprod:4d}')
+
+# we know that no one's original number was a len1
+# so for each len1 entry, we can remove it from each of the three dicts
+for n in range(1, 6):
+    print(f'End of hour {n}')
+    for adict in dicts:
+        adict.buildLen1KeyDict()
+        dbglog(f'{adict.name} len1KeyDict len: {len(adict.len1KeyDict)}')
+    for adict in dicts:
+        adict.show()
+
+    for adict in dicts:
+        # for each dict, go thru and find any len 1 items
+        # and remove them from all 3 dicts
+        for key, info in adict.len1KeyDict.items():
+            dbglog(f'{adict.name} len1List to remove {key}->{info.num}')
+            adict.removeFromAll3(key, info, prodDict, sumsqDict, sumprodDict)
+
